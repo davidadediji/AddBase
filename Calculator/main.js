04ba87df2bb6ddda2f36e7ec6a21db5f543de23a
Emma Paris <davidadediji@gmail.com>
2022-05-02 13:17:09 +0100

    add change

---
+++ Calculator/main.js
@@ -0,0 +1,9 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    
+});
+});
