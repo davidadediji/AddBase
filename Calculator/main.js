6e6befe1abc7301d41521a2624cda1f48a4025ce
Emma Paris <davidadediji@gmail.com>
2022-05-03 01:48:13 +0100

    apply changes

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,9 +1,22 @@
-let display = document.getElementById("display");
+let display = document.querySelector("#display .calculate");
+let result = document.querySelector("#display .result");
 
 let buttons = Array.from(document.getElementsByClassName("button"));
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    
-});
+    if (e.target.innerText === "C") {
+      display.innerText = result.innerText = "";
+    } else if (e.target.innerText === "←") {
+      display.innerText = result.innerText = display.innerText.slice(0, -1);
+    } else if (e.target.innerText === "=") {
+      try {
+        result.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Syntax Error !";
+      }
+    } else {
+      display.innerText += e.target.innerText;
+    }
+  });
 });
