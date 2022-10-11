73f2f3f1123da6cb1c52ceb99501cfb877804903
davidadediji <davidadediji@gmail.com>
2022-10-11 11:43:35 +0100

    chore:set up client

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box, Center, Flex, Text, VStack } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<Box >
+			<Center>
+				<Text>Movie Review Application</Text>
+			</Center>
+			<Flex direction={'column'} paddingLeft={56}>
+				<label htmlFor='movie-name'>Movie Name:</label>
+				<input
+					style={{ border: '1px solid #444', borderRadius: '7px', width:'400px'}}
+					type='text'
+					name='movie-name'
+					id=''
+				/>
+				<label htmlFor='movie-review'>Movie Review:</label>
+				<input
+					style={{ border: '1px solid #444', borderRadius: '7px', width:'400px' }}
+					type='text'
+					name='movie-review'
+					id=''
+				/>
+			</Flex>
+		</Box>
+	);
 }
 
-export default App
+export default App;
