d6f225be7420715012008fdefa5b2576d4de4e91
davidadediji <davidadediji@gmail.com>
2022-10-16 06:42:21 +0100

    chore:prettier

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('hello David');
+});
 
-app.get('/', (req, res)=>{
-    res.send('hello David')
-})
-
-
-app.listen(3001, ()=>{
-    console.log('application running on 3001');
-})
+app.listen(3001, () => {
+	console.log('application running on 3001');
+});
