01866d8b6edd674bfa9537f6c5b27e8cdf5282cb
davidadediji <davidadediji@gmail.com>
2022-11-01 16:47:22 +0100

    chore:implement post and get requests

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mysql = require('mysql2');
 const cors = require('cors');
-const parser = require('body-parser')
+const parser = require('body-parser');
 
 const app = express();
 dotenv.config();
 
-app.use(cors())
-app.use(parser.urlencoded({extended:false}))
-app.use(express());
+app.use(cors());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.json());
 
 const db = mysql.createConnection({
 	host: 'localhost',
@@ -30,7 +30,9 @@ const port = process.env.PORT || 4015;
 
 app.post('/api/insert', (req, res) => {
 	const sqlInsert =
-		'INSERT INTO movie_reviews (movie_name, movie_review VALUES(?,?))';
+		'INSERT INTO movie_reviews (movie_name, movie_review) VALUES(?, ?)';
+	const movieName = req.body.movieName;
+	const movieReview = req.body.movieReview;
 
 	db.query(sqlInsert, [movieName, movieReview], (err, result) => {
 		if (err) {
@@ -42,6 +44,23 @@ app.post('/api/insert', (req, res) => {
 
 	res.send('good');
 });
+
+app.get('/api/get', (req, res) => {
+	const sqlGet = 'SELECT * FROM movie_reviews';
+	db.query(sqlGet, (err, result) => {
+		if(err){
+			console.error(err)
+		}
+		res.send(result);
+	});
+});
+
+app.get('/api/get/:id', (req, res)=>{
+	const sqlGetSingle = `SELECT * FROM movie_reviews WHERE id=${req.params.id}`
+	db.query(sqlGetSingle, (err, result)=>{
+		res.send(result)
+	})
+})
 
 app.listen(port, () => {
 	console.log(`listening at port ${port}`);
