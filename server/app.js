e5da7fa13b8d45ae55296c6c7320d435b3fe4996
davidadediji <davidadediji@gmail.com>
2022-11-01 16:07:21 +0100

    chore:resolve cors issue

--- server/app.js
+++ server/app.js
@@ -1,40 +1,48 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
 const mysql = require('mysql2');
+const cors = require('cors');
+const parser = require('body-parser')
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
+app.use(cors())
+app.use(parser.urlencoded({extended:false}))
+app.use(express());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"dbMovieReviews"
-})  
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'dbMovieReviews',
+});
 
-db.connect((err, result)=>{
-    if(err){
-        console.log('no connection')
-    }else{
-        console.log('successful connection');
-    }
-})
+db.connect((err, result) => {
+	if (err) {
+		console.log('no connection');
+	} else {
+		console.log('successful connection');
+	}
+});
 
 const port = process.env.PORT || 4015;
 
-app.get('/', (req, res)=>{
-    const mysqlStatement = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES ('inception', 'intriguing')";
-    db.query(mysqlStatement, (err, result)=>{
-        res.end('hello world')
-        if (err){
-            console.log(err.name, err.message)
-        }
-    });
-})
+app.post('/api/insert', (req, res) => {
+	const sqlInsert =
+		'INSERT INTO movie_reviews (movie_name, movie_review VALUES(?,?))';
 
-app.listen(port, ()=>{
-    console.log(`listening at port ${port}`)
-})
+	db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(result);
+	});
 
+	res.send('good');
+});
 
+app.listen(port, () => {
+	console.log(`listening at port ${port}`);
+});
