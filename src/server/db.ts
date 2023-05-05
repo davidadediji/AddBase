0ec942e617fbd9772772f9f72bb5561d5acdf76e
davidadediji <davidadediji@gmail.com>
2023-05-05 17:52:45 +0100

    add prettier config

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,23 +4,21 @@ import { DATABASE_NAME, MONGODB_URI } from './config';
 let connectedClient;
 
 export const connectClient = async () => {
+  if (connectedClient) {
+    return connectedClient.db(DATABASE_NAME);
+  }
+  const client = new MongoClient(MONGODB_URI); //prepare a client
+  await client.connect();
 
-    if (connectedClient){
-        return connectedClient.db(DATABASE_NAME)
-    }
-	const client = new MongoClient(MONGODB_URI); //prepare a client
-	await client.connect();
+  //ping
+  await client.db(DATABASE_NAME).command({ ping: 1 });
+  console.info('Connected to MongoDB');
 
+  connectedClient = client;
 
-    //ping
-    await client.db(DATABASE_NAME).command({ping:1})
-    console.info("Connected to MongoDB")
-
-    connectedClient = client
-
-    return connectedClient.db(DATABASE_NAME)
+  return connectedClient.db(DATABASE_NAME);
 };
 
-export const stopClient = async()=>{
-    await connectedClient?.close();
-}
+export const stopClient = async () => {
+  await connectedClient?.close();
+};
